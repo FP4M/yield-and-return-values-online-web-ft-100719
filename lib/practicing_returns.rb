26b857f7da139322fa3fddbc3a16511a8d06b3ed
Prince Charles <princecharles35@yahoo.com>
2019-10-16 16:15:07 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,7 @@
-require 'pry'
-
-def hello(array)
+def hello('apple')
   i = 0
   while i < array.length
     yield(array[i])
     i += 1
   end
 end
-
-
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
